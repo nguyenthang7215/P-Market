614a0f4fc3d71d435cf3fa85c9292f0440803a42
Nguyen Thang <nguyenthang7215@gmail.com>
2025-10-26 10:54:29 +0700

    feat: update routers (auth, user)

--- backend/src/routers/userRouter.js
+++ backend/src/routers/userRouter.js
@@ -1,20 +1,31 @@
 import { Router } from 'express';
 import * as userController from '../app/controllers/userController.js';
+import * as userRequest from '../app/requests/userRequest.js';
+import validate from '../app/middleware/common/validate.js';
+import requireAuthentication from '../app/middleware/common/require-authentication.js';
+import { checkValidId } from '../app/middleware/common/valid-params.js';
 
 const userRouter = Router();
 
+userRouter.use(requireAuthentication);
+
 userRouter.post(
     '/register',
+    validate(userRequest.createUser),
     userController.createUser
 );
 
 userRouter.patch(
-    '/reset-password',
+    '/:id/reset-password',
+    checkValidId,
+    validate(userRequest.resetPassword),
     userController.resetPassword
 );
 
 userRouter.put(
     '/:id/update-profile',
+    checkValidId,
+    validate(userRequest.updateProfile),
     userController.updateProfile
 );
 
