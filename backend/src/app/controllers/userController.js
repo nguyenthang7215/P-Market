0ba450687b53819132fd53eb24cf7ea2b0da8da2
Nguyen Thang <nguyenthang7215@gmail.com>
2025-10-28 23:12:02 +0700

    feat: update controllers user for upload avatar

--- backend/src/app/controllers/userController.js
+++ backend/src/app/controllers/userController.js
@@ -13,6 +13,24 @@ export async function createUser(req, res) {
     });
 }
 
+export async function findUserByEmail(req, res) {
+    const user = await userService.findUserByEmail(req.params.email);
+
+    res.json({
+        success: true,
+        message: 'Người dùng tồn tại'
+    });
+}
+
+export async function findUserByUserName(req, res) {
+    const user = await userService.findUserByUserName(req.params.userName);
+
+    res.json({
+        success: true,
+        message: 'Người dùng tồn tại'
+    })
+}
+
 export async function resetPassword(req, res) {
     await userService.resetPassword(req.params.id, req.body.password);
 
@@ -22,10 +40,40 @@ export async function resetPassword(req, res) {
     });
 }
 
-export async function updateProfile(req, res) {
-    await userService.updateProfile(req.params.id, req.body);
+export async function updateUserName(req, res) {
+    await userService.updateUserName(req.params.id, req.body.userName);
+
     res.json({
         success: true,
-        message: 'Cập nhật thông tin người dùng thành công!'
+        message: 'Cập nhật UserName thành công!'
+    })
+}
+
+export async function updatePhone(req, res) {
+    await userService.updatePhone(req.params.id, req.body.phone);
+
+    res.json({
+        success: true,
+        message: 'Cập nhật số điện thoại thành công!'
     });
+}
+
+export async function updateAddress(req, res) {
+    await userService.updateAddress(req.params.id, req.body.address);
+
+    res.json({
+        success: true,
+        message: 'Cập nhật địa chỉ thành công!'
+    })
+}
+
+export async function uploadAvatar(req, res) {
+    const id = req.params.id;
+    const imagePath = `public/uploads/${req.file.filename}`;
+    await userService.uploadAvatar(id, imagePath);
+
+    res.json({
+        success: true,
+        message: 'Cập nhật ảnh thành công!'
+    })
 }
